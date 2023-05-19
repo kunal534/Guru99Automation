3cf98c6526ebb48915932703cc8151cffa1c6564
kunal534 <113521848+kunal534@users.noreply.github.com>
2023-05-19 17:39:29 +0530

    DemoGuru99.java

---
+++ DemoGuru99.java
@@ -0,0 +1,17 @@
+package Day7;
+
+public class DemoGuru99 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Guru99 d = new Guru99();
+		
+		d.invokeBrowser();
+		
+		d.login("mngr482301", "vUnamyt");// enter your own username and password
+		
+		d.addAccount("6398", "1000");
+		
+	}
+
+}
